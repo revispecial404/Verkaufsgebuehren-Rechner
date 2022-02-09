36ce30a33fa2ad398967cb5b7b84ab9386d5a818
revispecial404 <94785950+revispecial404@users.noreply.github.com>
2022-02-09 18:50:35 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\olive\\Documents\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\olive\\Flutter_Project\\rechner_verkaufsgebuehren\\rechner_verkaufsgebuehren" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\olive\\Documents\\flutter"
+  "PROJECT_DIR=C:\\Users\\olive\\Flutter_Project\\rechner_verkaufsgebuehren\\rechner_verkaufsgebuehren"
+  "FLUTTER_ROOT=C:\\Users\\olive\\Documents\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\olive\\Flutter_Project\\rechner_verkaufsgebuehren\\rechner_verkaufsgebuehren\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\olive\\Flutter_Project\\rechner_verkaufsgebuehren\\rechner_verkaufsgebuehren"
+  "FLUTTER_TARGET=C:\\Users\\olive\\Flutter_Project\\rechner_verkaufsgebuehren\\rechner_verkaufsgebuehren\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\olive\\Flutter_Project\\rechner_verkaufsgebuehren\\rechner_verkaufsgebuehren\\.dart_tool\\package_config.json"
+)
